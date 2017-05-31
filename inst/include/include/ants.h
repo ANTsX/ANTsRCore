a3eccb3630c40102a716121739ac1ee71e320d17
muschellij2 <muschellij2@gmail.com>
2017-05-30 21:59:32 -0400

    no registration on useDynlib

---
+++ inst/include/include/ants.h
@@ -0,0 +1,236 @@
+#ifndef ants_h
+#define ants_h
+
+#include "itkSurfaceCurvatureBase.h"
+
+#include "itkSurfaceImageCurvature.h"
+
+#include "itkAlternatingValueSimpleSubtractionImageFilter.h"
+
+#include "itkAverageOverDimensionImageFilter.h"
+
+#include "itkAlternatingValueDifferenceImageFilter.h"
+
+#include "antsAffineInitializer.h"
+
+#include "antsAI.h"
+
+#include "antsApplyTransforms.h"
+
+#include "antsAlignOrigin.h"
+
+#include "antsApplyTransformsToPoints.h"
+
+#include "antsJointFusion.h"
+
+#include "antsJointTensorFusion.h"
+
+#include "antsTransformInfo.h"
+
+#include "ANTSConformalMapping.h"
+
+#include "ANTS_.h"
+
+#include "ANTSIntegrateVectorField.h"
+
+#include "ANTSIntegrateVelocityField.h"
+
+#include "ANTSJacobian.h"
+
+#include "ants_moco.h"
+
+#include "antsMotionCorr.h"
+
+#include "antsMotionCorrStats.h"
+
+#include "antsMotionCorrDiffusionDirection.h"
+
+#include "antsSliceRegularizedRegistration.h"
+
+#include "antsRegistration.h"
+
+#include "antsSurf.h"
+
+#include "antsUtilitiesTesting.h"
+
+#include "antsVol.h"
+
+#include "ANTSUseDeformationFieldToGetAffineTransform.h"
+
+#include "ANTSUseLandmarkImagesToGetAffineTransform.h"
+
+#include "ANTSUseLandmarkImagesToGetBSplineDisplacementField.h"
+
+#include "antsLandmarkBasedTransformInitializer.h"
+
+#include "Atropos.h"
+
+#include "AverageAffineTransform.h"
+
+#include "AverageAffineTransformNoRigid.h"
+
+#include "AverageImages.h"
+
+#include "AverageTensorImages.h"
+
+#include "CheckTopology.h"
+
+#include "ClusterImageStatistics.h"
+
+#include "ComposeMultiTransform.h"
+
+#include "CompositeTransformUtil.h"
+
+#include "ComputeSimilarityMetric.h"
+
+#include "ConformalMapping.h"
+
+#include "ConvertImage.h"
+
+#include "ConvertImagePixelType.h"
+
+#include "ConvertInputImagePixelTypeToFloat.h"
+
+#include "ConvertScalarImageToRGB.h"
+
+#include "ConvertToJpg.h"
+
+#include "ConvertVectorFieldToVTK.h"
+
+#include "CopyImageHeaderInformation.h"
+
+#include "CreateDisplacementField.h"
+
+#include "CreateDTICohort.h"
+
+#include "CreateImage.h"
+
+#include "CreateJacobianDeterminantImage.h"
+
+#include "CreateTiledMosaic.h"
+
+#include "CreateWarpedGridImage.h"
+
+#include "DeNrrd.h"
+
+#include "DenoiseImage.h"
+
+#include "ExtractRegionFromImageByMask.h"
+
+#include "ExtractRegionFromImage.h"
+
+#include "ExtractSliceFromImage.h"
+
+#include "FitBSplineToPoints.h"
+
+#include "GetMeshAndTopology.h"
+
+#include "iMath.h"
+
+#include "ImageCompare.h"
+
+#include "ImageMath.h"
+
+#include "ImageIntensityStatistics.h"
+
+// #include "antsSimilarityInitializer.h"
+
+#include "ImageSetStatistics.h"
+
+#include "itkCommandLineParserTest.h"
+
+#include "KellyKapowski.h"
+
+#include "KellySlater.h"
+
+#include "LabelClustersUniquely.h"
+
+#include "LabelGeometryMeasures.h"
+
+#include "LabelOverlapMeasures.h"
+
+#include "LaplacianThickness.h"
+
+#include "LesionFilling.h"
+
+#include "MeasureImageSimilarity.h"
+
+#include "MeasureMinMaxMean.h"
+
+#include "MemoryTest.h"
+
+#include "MultiplyImages.h"
+
+#include "N3BiasFieldCorrection.h"
+
+#include "N4BiasFieldCorrection.h"
+
+#include "NonLocalSuperResolution.h"
+
+#include "PasteImageIntoImage.h"
+
+#include "PermuteFlipImageOrientationAxes.h"
+
+#include "PrintHeader.h"
+
+#include "RebaseTensorImage.h"
+
+#include "ReorientTensorImage.h"
+
+#include "ResampleImageBySpacing.h"
+
+#include "ResampleImage.h"
+
+#include "ResetDirection.h"
+
+#include "sccan.h"
+
+#include "simpleSynRegistration.h"
+
+#include "SetDirectionByMatrix.h"
+
+#include "SetOrigin.h"
+
+#include "SetSpacing.h"
+
+#include "SmoothImage.h"
+
+#include "SmoothDisplacementField.h"
+
+#include "StackSlices.h"
+
+#include "StudentsTestOnImages.h"
+
+#include "SuperResolution.h"
+
+#include "SurfaceBasedSmoothing.h"
+
+#include "SurfaceCurvature.h"
+
+#include "TensorDerivedImage.h"
+
+#include "ThresholdImage.h"
+
+#include "TileImages.h"
+
+#include "TimeSCCAN.h"
+
+#include "WarpImageMultiTransform.h"
+
+#include "WarpTensorImageMultiTransform.h"
+
+#include "WarpTimeSeriesImageMultiTransform.h"
+
+#include "WarpVTKPolyDataMultiTransform.h"
+
+#include "ConvertTransformFile.h"
+
+#include "compareTwoTransforms.h"
+
+#include "GetConnectedComponentsFeatureImages.h"
+
+#include "TextureRunLengthFeatures.h"
+
+#include "TextureCooccurrenceFeatures.h"
+
+#endif // ants_h
