273914a311a362fc91e8de5765e03b9685552350
stnava <stnava@gmail.com>
2017-06-30 12:59:44 -0400

    STYLE: do not add the ants repository.
    will need to rewrite the history to remove this unnecessary bloat.

--- make_comparison_operator.R
+++
@@ -1,7 +0,0 @@
-code = readLines("comparison_operator_template.R")
-comparators = c("==", ">", "<", "!=", "<=", ">=")
-res_code = lapply(comparators, function(iop){ 
-  c(gsub("OPERATOR", iop, code), rep("", 5))
-})
-res_code = unlist(res_code)
-writeLines(res_code, con = "R/zzz_Comparison.R")
