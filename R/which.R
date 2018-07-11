a91f9bf744d725343fc18e40295b13d75865f7e3
muschellij2 <muschellij2@gmail.com>
2018-07-11 14:02:38 -0400

    putting which in the docs

--- R/which.R
+++ R/which.R
@@ -5,6 +5,15 @@ which.antsImage <- function(x, arr.ind = FALSE, useNames = TRUE) {
 
 #' @aliases which,antsImage-method
 #' @export
+#' @examples 
+#' img = makeImage(c(10,10),rnorm(100))
+#' mask = img > 0
+#' which(mask)
+#' which(mask, arr.ind = TRUE)
+#' \dontrun{
+#' which(mask + 1)
+#' }
+#' @rdname antsImageops
 setMethod("which", "antsImage", which.antsImage)
 
 
