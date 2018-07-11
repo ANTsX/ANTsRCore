1a8ebc8ede39a1d9bf3a73294a335a8b554b24e7
muschellij2 <muschellij2@gmail.com>
2018-07-11 19:04:24 -0400

    added in the which opts

--- R/which.R
+++ R/which.R
@@ -5,6 +5,11 @@ which.antsImage <- function(x, arr.ind = FALSE, useNames = TRUE) {
 
 #' @aliases which,antsImage-method
 #' @export
+#' @param x a logical vector or array.
+#' @param arr.ind logical; should array indices be returned when x is an array?
+#' @param useNames logical indicating if the value of arrayInd() 
+#' should have (non-null) dimnames at all.
+#' 
 #' @examples 
 #' img = makeImage(c(10,10),rnorm(100))
 #' mask = img > 0
