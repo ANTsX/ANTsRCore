328c4ea2762e278e4602b9510f985caf2be0f922
muschellij2 <muschellij2@gmail.com>
2018-08-20 16:30:41 -0400

    fixed conflicts

--- R/int_antsProcessArguments.R
+++ R/int_antsProcessArguments.R
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 #' Processing arguments for command line parsing
 #' 
 #' @rdname int_antsProcessArguments
 #' @param args arguments to parse into those for \code{ANTsR} functions
 #' @return A character vector
-=======
 #' args = "hey"
 #' .int_antsProcessArguments(args)
 #' args = list(h = "hey")
@@ -12,7 +10,6 @@
 #' args = list(h = list("hey"))
 #' .int_antsProcessArguments(args)
 #' @rdname int_antsProcessArguments
->>>>>>> master
 .int_antsProcessArguments <- function(args) {
   char_vect <- ""
   if (typeof(args) == "list") {
